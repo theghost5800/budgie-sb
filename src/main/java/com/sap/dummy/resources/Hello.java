d7bb0ac2d815dc555437f2f266b8592fba024f21
Alexander Tsvetkov <alexander.tsvetkov.93@gmail.com>
2018-04-23 14:04:19 +0300

    Add hello resource

---
+++ src/main/java/com/sap/dummy/resources/Hello.java
@@ -0,0 +1,14 @@
+package com.sap.dummy.resources;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+
+@Path("/hello")
+public class Hello {
+
+    @GET
+    public String getMessage() {
+        return "Hello, world!";
+    }
+
+}

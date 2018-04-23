8821d1e2778a4feb49153af6500fe659c29a2fa5
Alexander Tsvetkov <alexander.tsvetkov.93@gmail.com>
2018-04-23 16:35:16 +0300

    Add Gson message body writer
    This allows us to use Gson annotations on domain objects.

---
+++ src/main/java/com/sap/dummy/providers/GsonMessageBodyWriter.java
@@ -0,0 +1,48 @@
+package com.sap.dummy.providers;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.lang.annotation.Annotation;
+import java.lang.reflect.Type;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+
+import javax.ws.rs.Produces;
+import javax.ws.rs.WebApplicationException;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.MultivaluedMap;
+import javax.ws.rs.ext.MessageBodyWriter;
+import javax.ws.rs.ext.Provider;
+
+import com.google.gson.Gson;
+
+@Provider
+@Produces(MediaType.APPLICATION_JSON)
+public class GsonMessageBodyWriter<T> implements MessageBodyWriter<T> {
+
+    @Override
+    public long getSize(T t, Class<?> type, Type genericType, Annotation[] annotations, MediaType mediaType) {
+        return -1;
+    }
+
+    @Override
+    public void writeTo(T t, Class<?> type, Type genericType, Annotation[] annotations, MediaType mediaType,
+        MultivaluedMap<String, Object> httpHeaders, OutputStream entityStream) throws IOException, WebApplicationException {
+
+        String stringJson = new Gson().toJson(t);
+        byte[] binaryJson = stringJson.getBytes(getCharset(mediaType));
+        entityStream.write(binaryJson);
+    }
+
+    private Charset getCharset(MediaType mediaType) {
+        String charsetName = mediaType.getParameters()
+            .get(MediaType.CHARSET_PARAMETER);
+        return charsetName == null ? StandardCharsets.UTF_8 : Charset.forName(charsetName);
+    }
+
+    @Override
+    public boolean isWriteable(Class<?> type, Type genericType, Annotation[] annotations, MediaType mediaType) {
+        return true;
+    }
+
+}

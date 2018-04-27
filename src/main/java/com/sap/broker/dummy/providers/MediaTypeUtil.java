b5605eefca0f65d9ef8e144e10318319d9e527b4
Alexander Tsvetkov <alexander.tsvetkov.93@gmail.com>
2018-04-27 22:25:56 +0300

    Handle null media types in message body writers/readers

--- src/main/java/com/sap/broker/dummy/providers/MediaTypeUtil.java
+++ src/main/java/com/sap/broker/dummy/providers/MediaTypeUtil.java
@@ -2,15 +2,23 @@ package com.sap.broker.dummy.providers;
 
 import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
+import java.util.Map;
 
 import javax.ws.rs.core.MediaType;
 
 public class MediaTypeUtil {
 
     public static Charset getCharset(MediaType mediaType) {
-        String charsetName = mediaType.getParameters()
-            .get(MediaType.CHARSET_PARAMETER);
-        return charsetName == null ? StandardCharsets.UTF_8 : Charset.forName(charsetName);
+        String charset = getCharsetParameter(mediaType);
+        return charset != null ? Charset.forName(charset) : StandardCharsets.UTF_8;
+    }
+
+    private static String getCharsetParameter(MediaType mediaType) {
+        if (mediaType == null) {
+            return null;
+        }
+        Map<String, String> mediaTypeParameters = mediaType.getParameters();
+        return mediaTypeParameters.get(MediaType.CHARSET_PARAMETER);
     }
 
 }

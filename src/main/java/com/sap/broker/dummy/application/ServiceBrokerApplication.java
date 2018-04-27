676ea365d9678cbc28ef25782a2875fd4c9da2f8
Alexander Tsvetkov <alexander.tsvetkov.93@gmail.com>
2018-04-27 17:54:29 +0300

    Transform exception into broker errors

--- src/main/java/com/sap/broker/dummy/application/ServiceBrokerApplication.java
+++ src/main/java/com/sap/broker/dummy/application/ServiceBrokerApplication.java
@@ -8,6 +8,7 @@ import javax.ws.rs.core.Application;
 
 import com.sap.broker.dummy.providers.GsonMessageBodyReader;
 import com.sap.broker.dummy.providers.GsonMessageBodyWriter;
+import com.sap.broker.dummy.providers.ServiceBrokerExceptionMapper;
 import com.sap.broker.dummy.resources.api.CatalogResource;
 import com.sap.broker.dummy.resources.api.ServiceInstancesResource;
 
@@ -21,6 +22,7 @@ public class ServiceBrokerApplication extends Application {
         classes.add(ServiceInstancesResource.class);
         classes.add(GsonMessageBodyWriter.class);
         classes.add(GsonMessageBodyReader.class);
+        classes.add(ServiceBrokerExceptionMapper.class);
         return classes;
     }
 
---
+++ src/main/java/com/sap/broker/dummy/exception/ConflictException.java
@@ -0,0 +1,23 @@
+package com.sap.broker.dummy.exception;
+
+public class ConflictException extends RuntimeException {
+
+    private static final long serialVersionUID = 1L;
+
+    public ConflictException() {
+        super();
+    }
+
+    public ConflictException(String message) {
+        super(message);
+    }
+
+    public ConflictException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public ConflictException(Throwable cause) {
+        super(cause);
+    }
+
+}

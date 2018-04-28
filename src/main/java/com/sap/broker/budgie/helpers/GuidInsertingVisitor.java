8dfe2664922dfeb011718403c2433a1eb3c7dafb
Alexander Tsvetkov <alexander.tsvetkov.93@gmail.com>
2018-04-28 23:18:07 +0300

    Do not generate UUIDs for unnamed services and plans

--- src/main/java/com/sap/broker/budgie/helpers/GuidInsertingVisitor.java
+++ src/main/java/com/sap/broker/budgie/helpers/GuidInsertingVisitor.java
@@ -1,6 +1,7 @@
 package com.sap.broker.budgie.helpers;
 
 import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
 import java.util.UUID;
 
 import com.sap.broker.budgie.domain.Plan;
@@ -24,8 +25,17 @@ public class GuidInsertingVisitor implements Visitor {
     }
 
     private UUID generateGuid(String... names) {
-        String name = String.join("", names);
-        return UUID.nameUUIDFromBytes(name.getBytes(StandardCharsets.UTF_8));
+        return containsNullOrEmptyElements(names) ? null : UUID.nameUUIDFromBytes(getBytes(names));
+    }
+
+    private byte[] getBytes(String... names) {
+        String concatenatedNames = String.join("", names);
+        return concatenatedNames.getBytes(StandardCharsets.UTF_8);
+    }
+
+    private boolean containsNullOrEmptyElements(String... strings) {
+        return Arrays.stream(strings)
+            .anyMatch(string -> string == null || string.isEmpty());
     }
 
 }
---
+++ src/test/java/com/sap/broker/budgie/helpers/GuidInsertingVisitorTest.java
@@ -0,0 +1,29 @@
+package com.sap.broker.budgie.helpers;
+
+import static org.junit.jupiter.api.Assertions.assertNull;
+
+import java.util.Arrays;
+import java.util.Collections;
+
+import org.junit.jupiter.api.Test;
+
+import com.sap.broker.budgie.domain.Catalog;
+import com.sap.broker.budgie.domain.Service;
+
+public class GuidInsertingVisitorTest {
+
+    private GuidInsertingVisitor visitor = new GuidInsertingVisitor();
+
+    @Test
+    public void testGuidsAreNotGeneratedForNullOrEmptyNames() {
+        Service serviceA = new Service(null, null, null, true, Collections.emptyList());
+        Service serviceB = new Service(null, "", null, true, Collections.emptyList());
+        Catalog catalog = new Catalog(Arrays.asList(serviceA, serviceB));
+
+        catalog.accept(visitor);
+
+        assertNull(serviceA.getId());
+        assertNull(serviceB.getId());
+    }
+
+}

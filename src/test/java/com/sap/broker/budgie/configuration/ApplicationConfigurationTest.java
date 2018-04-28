8a91e72daf4a8592e299fe2ccd33deb65eca2ece
Alexander Tsvetkov <alexander.tsvetkov.93@gmail.com>
2018-04-28 10:28:13 +0300

    Set up JUnit 5

---
+++ src/test/java/com/sap/broker/budgie/configuration/ApplicationConfigurationTest.java
@@ -0,0 +1,12 @@
+package com.sap.broker.budgie.configuration;
+
+import org.junit.jupiter.api.Test;
+
+public class ApplicationConfigurationTest {
+
+    @Test
+    public void test() {
+        System.out.println("123");
+    }
+
+}
